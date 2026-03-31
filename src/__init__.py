44201f3ac027e3e3be882336bcb1ceec2c938253
leonardotideveloper <leonardoti.dev@gmail.com>
2026-03-31 20:13:52 -0300

    chore: initial project setup with requirements.txt and .env.example

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from src.config import settings
+from src.controllers import (
+    EasyController,
+    ExtremeController,
+    HardController,
+)
+
+__all__ = [
+    "EasyController",
+    "ExtremeController",
+    "HardController",
+    "settings",
+]
