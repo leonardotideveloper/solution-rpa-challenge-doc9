69019b856c6e334289fabda3185ff1d694fea266
leonardotideveloper <leonardoti.dev@gmail.com>
2026-03-31 21:05:10 -0300

    refactor: Remove unused imports from init.py files

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,12 +1,9 @@
-from .easy_controller import EasyController, run_easy_controller
-from .extreme_controller import ExtremeController, run_extreme_controller
-from .hard_controller import HardController, run_hard_controller
+from .easy_controller import EasyController
+from .extreme_controller import ExtremeController
+from .hard_controller import HardController
 
 __all__ = [
     "EasyController",
     "ExtremeController",
     "HardController",
-    "run_easy_controller",
-    "run_extreme_controller",
-    "run_hard_controller",
 ]
